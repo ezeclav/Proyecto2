e72aedcdb3f2cc660fbc6cee675df0cf242ee70b
GustavoBozzano <gustavojorgebozzano@gmail.com>
2023-12-13 18:25:48 +0100

    corrección de errores

--- index.js
+++ index.js
@@ -5,11 +5,3 @@ const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
-import Express from "express";
-
-const app = express();
-app.set("port", 4000);
-app.listen(app.get(port));
-
-console.log(`servidor corriendo en el puerto ${app}`)
