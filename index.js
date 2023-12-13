0f226a08bb2725b9e6617550cff3ff2588fa1d34
Fiorella <fioremartinez99@hotmail.com>
2023-12-13 18:55:09 +0100

    arreglo de incidencia

--- index.js
+++ index.js
@@ -6,10 +6,3 @@ server.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
 
-import Express from "express";
-
-const app = express();
-app.set("port", 4000);
-app.listen(app.get(port));
-
-console.log(`servidor corriendo en el puerto ${app}`)
