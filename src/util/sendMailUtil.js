9ee87399d3e5453c102652bc27c7c20f485458a7
GustavoBozzano <gustavojorgebozzano@gmail.com>
2023-12-14 01:21:38 +0100

    solucion a problema de mail

--- src/util/sendMailUtil.js
+++ src/util/sendMailUtil.js
@@ -1,35 +1,38 @@
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
-import { sendMailError } from '../services/errorService.js';
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import { sendMailError } from "../services/errorService.js";
 
 dotenv.config();
 
-const {SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS} = process.env;
+const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASS } = process.env;
 
 const transport = nodemailer.createTransport({
-    host: SMTP_HOST,
-    port: SMTP_PORT,
-    auth:{
-        user: SMTP_USER,
-        pass: SMTP_PASS
-    }
+  host: SMTP_HOST,
+  port: SMTP_PORT,
+  secure: true,
+  auth: {
+    user: SMTP_USER,
+    pass: SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 const sendMailUtil = async (email, subject, body) => {
-    try {
-        const mailOptions = {
-            from: SMTP_USER,
-            to: email,
-            subject,
-            text: body
-        };
+  try {
+    const mailOptions = {
+      from: SMTP_USER,
+      to: email,
+      subject,
+      text: body,
+    };
 
-        await transport.sendMail(mailOptions);
-
-    } catch (error) {
-        console.log(error);
-        sendMailError();
-    }
-}
+    await transport.sendMail(mailOptions);
+  } catch (error) {
+    console.log(error);
+    sendMailError();
+  }
+};
 
 export default sendMailUtil;
