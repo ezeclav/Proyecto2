4d0537aa2491c5575e604c7f8b18c96bc8afe868
GustavoBozzano <gustavojorgebozzano@gmail.com>
2024-01-22 18:07:44 +0100

    arreglo en MODIF EXERCISE

--- src/controllers/exercises/modifExercisescontroller.js
+++ src/controllers/exercises/modifExercisescontroller.js
@@ -3,7 +3,10 @@ import jwt from "jsonwebtoken";
 import modifExerciseModel from "../../models/exercises/modifExerciseModel.js";
 import { emptyFieldExerciseError } from "../../services/errorService.js";
 import insertPhotoModel from "../../models/exercises/insertPhotoModel.js";
-import { savePhotoService } from "../../services/photoService.js";
+import {
+  savePhotoService,
+  deletePhotoService,
+} from "../../services/photoService.js";
 
 const modifExercisesController = async (req, res, next) => {
   try {
@@ -13,29 +16,33 @@ const modifExercisesController = async (req, res, next) => {
     if (!name && !description && !typology && !muscle_group && !equipment)
       emptyFieldExerciseError;
 
+    // let photos = [];
+    let photoName = "";
+
+    if (req.files) {
+      for (let photo of Object.values(req.files).slice(0, 1)) {
+        photoName = await savePhotoService(photo, 500);
+
+        // const photoId = await insertPhotoModel(photoName, exerciseId);
+        // photos.push({
+        //   id: photoId,
+        //   name: photoName,
+        // });
+      }
+    }
+
     const exercise = await modifExerciseModel(
       exerciseId,
       name,
       description,
       typology,
       muscle_group,
-      equipment
+      equipment,
+      photoName
     );
 
-    let photos = [];
-
-    if (req.files) {
-      for (let photo of Object.values(req.files).slice(0, 1)) {
-        let photoName = await savePhotoService(photo, 500);
-
-        const photoId = await insertPhotoModel(photoName, exerciseId);
-
-        photos.push({
-          id: photoId,
-          name: photoName,
-        });
-      }
-    }
+    const { name: imgName } = exercise[0];
+    await deletePhotoService(imgName);
 
     res.send({
       status: "ok",
--- src/models/exercises/modifExerciseModel.js
+++ src/models/exercises/modifExerciseModel.js
@@ -1,26 +1,44 @@
 import getPool from "../../database/getPool.js";
 
 const modifExerciseModel = async (
-  id_exercise,
+  exerciseId,
   name,
   description,
   typology,
   muscle_group,
-  equipment
+  equipment,
+  photoName
 ) => {
   const pool = await getPool();
 
+  const [namePhoto] = await pool.query(
+    `
+        SELECT name FROM photo_exercises
+        WHERE exerciseId = ${exerciseId} 
+    `
+  );
+
+  const [photo] = await pool.query(
+    `
+        UPDATE photo_exercises
+        SET name = ?, exerciseId = ?
+        WHERE exerciseId = ${exerciseId} 
+    `,
+    [photoName, exerciseId]
+  );
+
   const [result] = await pool.query(
     `
-            
             UPDATE exercises
             SET name = ?, description = ?, typology = ?, muscle_group = ?, equipment = ?
-            WHERE id_exercise = ${id_exercise}
+            WHERE id_exercise = ${exerciseId}
         `,
     [name, description, typology, muscle_group, equipment]
   );
 
-  console.log(result);
+  // console.log(result);
+
+  return namePhoto;
 };
 
 export default modifExerciseModel;
