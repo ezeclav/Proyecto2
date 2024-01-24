6a80ee7fa7442b05d64b49fdaa08d917378215b9
GustavoBozzano <gustavojorgebozzano@gmail.com>
2024-01-24 19:54:27 +0100

    ultima moidf en query params Exercise

--- src/models/exercises/selectExercisesByQueryParamsModel.js
+++ src/models/exercises/selectExercisesByQueryParamsModel.js
@@ -19,44 +19,26 @@ const selectExercisesByQueryParamsModel = async (params) => {
       queryParams.push(params.typology);
     }
 
-    console.log("consulta SQL:", query);
-    console.log("parametros:", queryParams);
     // Ejecutar la consulta SQL con los parámetros
     const [exercises] = await pool.query(query, queryParams);
+
+    for (const exercise of exercises) {
+      const [photos] = await pool.query(
+        `
+        SELECT id_photo_exercise, name FROM photo_exercises WHERE exerciseId=?
+      `,
+        [exercise.id_exercise]
+      );
+      // console.log(photos);
+
+      exercise.photos = photos;
+    }
 
     return exercises;
   } catch (error) {
     console.error("Error al obtener los ejercicios:", error.message);
     throw error;
   }
-  // finally {
-  //   await pool.end();
-  // }
 };
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// const [exercises] = await pool.query(
-//   `
-//             SELECT id_exercise, e.name, e.typology, e.muscle_group, e.equipment, COALESCE(COUNT(l.id_like_exercise), 0) AS likes
-//             FROM exercises e
-//             LEFT JOIN like_exercises l ON l.id_like_exercise = e.id_exercise
-//             GROUP BY e.id_exercise
-//             ORDER BY e.name DESC
-//     `
-// );
-
-// for (const exercise of exercises) {
-//   const [photos] = await pool.query(
-//     `
-//         SELECT id_photo_exercise, name FROM photo_exercises WHERE exerciseId=?
-//       `,
-//     [exercise.id_exercise]
-//   );
-//   // console.log(photos);
-
-//   exercise.photos = photos;
-// }
-
-// return exercises;
 
 export default selectExercisesByQueryParamsModel;
